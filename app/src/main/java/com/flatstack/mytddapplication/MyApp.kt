cd2ccf5eefe0dcfa4ec5cf68356e417473eaf47b
ILMAZ <ilmaz1309@gmail.com>
2018-12-21 00:48:06 +0300

    Added fabric (#6)

--- app/src/main/java/com/flatstack/mytddapplication/MyApp.kt
+++ app/src/main/java/com/flatstack/mytddapplication/MyApp.kt
@@ -1,9 +1,16 @@
 package com.flatstack.mytddapplication
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
 import com.flatstack.mytddapplication.di.initKodein
+import io.fabric.sdk.android.Fabric
 import org.kodein.di.KodeinAware
 
 class MyApp : Application(), KodeinAware {
     override val kodein = initKodein(this)
+
+    override fun onCreate() {
+        super.onCreate()
+        Fabric.with(this, Crashlytics())
+    }
 }
