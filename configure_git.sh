02c84d351acbe17e117c2e7648d7802b704c69e9
ILMAZ13 <ilmaz1309@gmail.com>
2018-12-21 01:32:20 +0300

    Test

--- configure_git.sh
+++ configure_git.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-git config --global filter.private.clean "openssl enc -aes-256-cbc -nosalt -a -k $SECURE_PASSWORD"
-git config --global filter.private.smudge "openssl enc -d -aes-256-cbc -nosalt -a -k $SECURE_PASSWORD"
-git config --global diff.private.textconv "openssl enc -d -aes-256-cbc -nosalt -a -k $SECURE_PASSWORD || cat"
+git config filter.private.clean "openssl enc -aes-256-cbc -nosalt -a -k $SECURE_PASSWORD"
+git config filter.private.smudge "openssl enc -d -aes-256-cbc -nosalt -a -k $SECURE_PASSWORD"
+git config diff.private.textconv "openssl enc -d -aes-256-cbc -nosalt -a -k $SECURE_PASSWORD || cat"
+git checkout
