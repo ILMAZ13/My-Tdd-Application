1d4c8bd2403db754d0f092db1e2a73ebd5cdb1a6
ILMAZ13 <ilmaz1309@gmail.com>
2018-12-21 01:18:53 +0300

    Test

--- configure_git.sh
+++ configure_git.sh
@@ -2,4 +2,3 @@
 git config filter.private.clean "openssl enc -aes-256-cbc -nosalt -a -k $SECURE_PASSWORD"
 git config filter.private.smudge "openssl enc -d -aes-256-cbc -nosalt -a -k $SECURE_PASSWORD"
 git config diff.private.textconv "openssl enc -d -aes-256-cbc -nosalt -a -k $SECURE_PASSWORD || cat"
-git checkout
