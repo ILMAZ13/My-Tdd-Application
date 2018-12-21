066ec3d8bbefdc3d418932b029817c4cc682101b
ILMAZ13 <ilmaz1309@gmail.com>
2018-12-21 13:59:46 +0300

    Sh Fixed

--- configure_git.sh
+++ configure_git.sh
@@ -2,4 +2,7 @@
 git config filter.private.clean "openssl enc -aes-256-cbc -nosalt -a -k $SECURE_PASSWORD"
 git config filter.private.smudge "openssl enc -d -aes-256-cbc -nosalt -a -k $SECURE_PASSWORD"
 git config diff.private.textconv "openssl enc -d -aes-256-cbc -nosalt -a -k $SECURE_PASSWORD || cat"
-git checkout
+git stash save
+rm .git/index
+git checkout HEAD -- "$(git rev-parse --show-toplevel)"
+git stash pop
