dc8052060bdc172466ee4c0c7f86dafbc54fcdca
ILMAZ13 <ilmaz1309@gmail.com>
2018-12-22 02:35:50 +0300

    Trying to fix

--- configure_git.sh
+++ configure_git.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-git config filter.private.clean "openssl enc -aes-256-cbc -nosalt -a -k $SECURE_PASSWORD"
-git config filter.private.smudge "openssl enc -d -aes-256-cbc -nosalt -a -k $SECURE_PASSWORD"
-git config diff.private.textconv "openssl enc -d -aes-256-cbc -nosalt -a -k $SECURE_PASSWORD || cat"
-git checkout
+git config filter.private.clean  "openssl enc    -aes-256-cbc -md md5 -nosalt -a -pass pass:$SECURE_PASSWORD"
+git config filter.private.smudge "openssl enc -d -aes-256-cbc -md md5 -nosalt -a -pass pass:$SECURE_PASSWORD || cat"
+git config diff.private.textconv "openssl enc -d -aes-256-cbc -md md5 -nosalt -a -pass pass:$SECURE_PASSWORD || cat"
+git stash save
+rm .git/index
+git checkout HEAD -- "$(git rev-parse --show-toplevel)"
+git stash pop
