cafc7ffc0671d1cd1af8e605c33b3ff5ac716a52
ILMAZ <ilmaz1309@gmail.com>
2018-12-23 00:10:01 +0300

    Trying to fix (#16)
    * Trying to fix
    
    * sh fix
    
    * No cats
    
    * Test to sha256
    
    * Changed to new standart

--- configure_git.sh
+++ configure_git.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-git config filter.private.clean "openssl enc -aes-256-cbc -nosalt -a -k $SECURE_PASSWORD"
-git config filter.private.smudge "openssl enc -d -aes-256-cbc -nosalt -a -k $SECURE_PASSWORD"
-git config diff.private.textconv "openssl enc -d -aes-256-cbc -nosalt -a -k $SECURE_PASSWORD || cat"
+git config filter.private.clean  "openssl aes-256-cbc -e -md sha256 -nosalt -a -pass pass:$SECURE_PASSWORD"
+git config filter.private.smudge "openssl aes-256-cbc -d -md sha256 -nosalt -a -pass pass:$SECURE_PASSWORD"
+git config diff.private.textconv "openssl aes-256-cbc -d -md sha256 -nosalt -a -pass pass:$SECURE_PASSWORD"
 git stash save
 rm .git/index
 git checkout HEAD -- "$(git rev-parse --show-toplevel)"
