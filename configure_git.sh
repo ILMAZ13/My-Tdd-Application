dbc0ecf065010db3b9a4f291f629873e959aa0b4
ILMAZ <ilmaz1309@gmail.com>
2019-01-18 11:54:40 +0300

    Added stash saving (#19)

--- configure_git.sh
+++ configure_git.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
-read -r -p "All your local changes will lost. Are you sure? [y/N] " response
+read -r -p "All your local changes can be lost. Are you sure? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
     git config filter.private.clean  "openssl aes-256-cbc -e -md sha256 -nosalt -a -pass pass:$SECURE_PASSWORD"
     git config filter.private.smudge "openssl aes-256-cbc -d -md sha256 -nosalt -a -pass pass:$SECURE_PASSWORD"
     git config diff.private.textconv "openssl aes-256-cbc -d -md sha256 -nosalt -a -pass pass:$SECURE_PASSWORD"
+    git stash save
     rm .git/index
     git checkout HEAD -- "$(git rev-parse --show-toplevel)"
+    git stash pop
     echo Done.
 else
     echo Rejected.
